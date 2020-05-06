2ff48217f408b741eb8d3355cbb5aba824e79c27
dengzhenzhen <zhenzhendeng2012@gmail.com>
2020-05-06 19:42:27 +0800

    7进制

--- hello.go
+++ hello.go
@@ -5,8 +5,5 @@ import "fmt"
 //main function here
 //Each solution owns one file
 func main() {
-	target := 12
-	position := []int{10, 8, 0, 5, 3}
-	speed := []int{2, 4, 1, 1, 3}
-	fmt.Println(carFleet(target, position, speed))
+	fmt.Println(convertToBase7(0))
 }
